ddbe5d01c9155b7b18a634a453bdb394c6735c5d
MukeshMishraReloaded <131019954+MukeshMishraReloaded@users.noreply.github.com>
2024-02-10 22:45:37 +0530

    Update churn_pred_ml_app.py

--- churn_pred_ml_app.py
+++ churn_pred_ml_app.py
@@ -2,56 +2,48 @@ import pandas as pd
 import streamlit as st
 import datetime
 
-
 import pickle
 
-cars_df = pd.read_csv("./cars24-car-price.csv")
+cars_df = pd.read_csv("./churn_logistic.csv")
 
 st.write("""
-# Cars24 Used Car Price Prediction
+# AT&T Subscriber Churn Prediction
 """)
 
-
-
-
-encode_dict = {
-    "fuel_type": {"Diesel": 1, "Petrol": 2, "CNG": 3, "LPG": 4, "Electric": 5},
-    "seller_type": {"Dealer":1,"Individual":2,"Trustmark Dealer":3  },
-    "transmission_type": {"Manual":1,"Automatic":2  }
-}
-
-def model_pred(fuel_type, transmission_type, engine, seats):
+def model_pred(cust_state, custsvc_calls, intl_plan, day_charge, day_mins):
 
     ##loading the model
-    with open("car_pred", "rb") as file:
+    with open("churn_prediction.pkl", "rb") as file:
         reg_model = pickle.load(file)
 
-    input_features = [[2018.0, 1, 40000, fuel_type, transmission_type, 19.70, engine, 86.30, seats]]
+    input_features = [[cust_state, custsvc_calls, intl_plan, day_charge, day_mins]]
     return reg_model.predict(input_features)
 
+col1, col2, col3 = st.columns(3)
 
-col1, col2 = st.columns(2)
+cust_state = col1.selectbox("State",
+                            [  'KS', 'OH', 'NJ', 'OK', 'AL', 'MA', 'MO', 'LA', 'WV', 'IN', 'RI',
+                               'IA', 'MT', 'NY', 'ID', 'VT', 'VA', 'TX', 'FL', 'CO', 'AZ', 'SC',
+                               'NE', 'WY', 'HI', 'IL', 'NH', 'GA', 'AK', 'MD', 'AR', 'WI', 'OR',
+                               'MI', 'DE', 'UT', 'CA', 'MN', 'SD', 'NC', 'WA', 'NM', 'NV', 'DC',
+                               'KY', 'ME', 'MS', 'TN', 'PA', 'CT', 'ND'
+                            ]
+                           )
 
-fuel_type = col1.selectbox("Select fuel type",
-                            ["Diesel", "Petrol", "CNG", "LPG", "Electric"])
+custsvc_calls = col2.selectbox("CustServ Calls",
+                            [0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
 
+intl_plan = col3.slider("Intl. Plan",
+                        [0, 1])
 
-engine = col1.slider("Set the engine power",
-                        500, 5000, step=100)
-
-transmission_type = col2.selectbox("Select transmission type",
-                                    ['Manual', "Automatic"])
-
-seats = col2.selectbox("No. of Seats",
-                        [4,5,6,7])
-
-if(st.button("Predict Price")):
-    fuel_type = encode_dict['fuel_type'][fuel_type]
-    transmission_type = encode_dict['transmission_type'][transmission_type]
-
-    price = model_pred(fuel_type, transmission_type, engine, seats)
-    st.text("Predicted price of the car: "+ str(price))
-
+day_charge = col1.slider("Day Charge",
+                        0, 59.64, step=7.5)
+day_mins = col2.slider("Day Mins.",
+                        0, 350.80, step=10)
 
 
-st.dataframe(cars_df.head())
+if(st.button("Predict Churn")):
+    pr = model_pred(cust_state, custsvc_calls, intl_plan, day_charge, day_mins)
+    st.text("Predicted churn value is: "+ str(pr))
+
+st.dataframe(churn.head(2))
