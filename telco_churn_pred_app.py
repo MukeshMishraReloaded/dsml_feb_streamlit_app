1a465b11b29e1f0eb84163905e5fa4a9a3030f8b
Mukesh Mishra <131019954+MukeshMishraReloaded@users.noreply.github.com>
2024-02-16 09:59:11 +0530

    Add files via upload

---
+++ telco_churn_pred_app.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import streamlit as st
+import datetime
+import pickle
+import numpy as np  # Ensure you import NumPy
+
+churn = pd.read_csv("./churn_logistic.csv")
+
+st.write("""
+# Telco churn predictor
+""")
+
+def model_pred(day_mins, eve_mins, night_mins, custsvc_calls, intl_plan, account_length):
+    # Convert input parameters to numeric types directly in Streamlit widgets
+    
+    # Load the model
+    with open("churn_prediction.pkl", "rb") as file:
+        reg_model = pickle.load(file)
+    
+    # Ensure the order of features matches the model's training order
+    input_features = np.array([[day_mins, eve_mins, night_mins, custsvc_calls, intl_plan, account_length]])
+    
+    return reg_model.predict(input_features)
+
+col1, col2 = st.columns(2)
+
+with col1:
+    custsvc_calls = st.selectbox("Calls made to customer service: ", [0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+
+with col2:
+    intl_plan = st.selectbox("Does the subscriber have an Intl. Plan?", [0, 1])
+
+day_mins = st.slider("Day Minutes", 0.0, 350.80, step=10.0)
+eve_mins = st.slider("Eve Minutes", 0.0, 363.70, step=7.5)
+night_mins = st.slider("Night Minutes", 0.0, 395.00, step=7.5)
+account_length = st.slider("Age of user account(in months): ", 1, 243, step=4)
+
+if st.button("Predict Churn"):
+    pr = model_pred(day_mins, eve_mins, night_mins, custsvc_calls, intl_plan, account_length)
+    if pr == 0:
+        st.subheader("This subscriber is unlikely to churn!")
+    else:
+        st.subheader("This subscriber may be considering exiting the service!")
+#st.dataframe(churn.head(2))
